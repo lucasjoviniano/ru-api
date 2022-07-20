18c640d05c329a00e7ae54cfddc6744ed0928c8d
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-07-20 12:26:05 -0300

    Add safety check in case page takes too much time to load

--- src/routes/campi.ts
+++ src/routes/campi.ts
@@ -3,18 +3,28 @@ import RUService from '../services/RUService';
 
 const router = express.Router()
 
+type Meal = {
+    'tipo': string,
+    'cardapio': string[]
+}
+
 router.get("/vicosa", async (request, response) => {
     const service = new RUService()
-
-    const vicosa = await service.campusVicosa("https://cardapio.ufv.br/");
+    let vicosa: Meal[] | undefined = []
+    do {
+        vicosa = await service.campusVicosa("https://cardapio.ufv.br/");
+    } while (vicosa?.length === 0)
 
     return response.json(vicosa)
 })
 
 router.get("/crp", async (request, response) => {
     const service = new RUService()
-
-    const crp = await service.campusCRP('https://sisru.crp.ufv.br/cardapioIframe.php')
+    let crp: Meal[] | undefined = [];
+    do {
+        crp = await service.campusCRP('https://sisru.crp.ufv.br/cardapioIframe.php')
+    } while (crp?.length === 0)
+    
 
     return response.json(crp)
 })
