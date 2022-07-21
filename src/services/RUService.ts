bb068c6ed5157229389451951ebc95384dc3abd1
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-07-21 14:15:12 -0300

    Refactor data processing

--- src/services/RUService.ts
+++ src/services/RUService.ts
@@ -27,86 +27,70 @@ class RUService {
         return pageData.html
     }
 
-    public async campusVicosa(url: string) {
+    private getMeals(html: string, separator: string = ':') {
+        const indexes: number[] = []
         const meals: string[][][] = []
         const json: Meal[] = []
-        const indexes: number[] = []
+
+        const $ = cheerio.load(html)
+        const element = $('#tbl_info tr td')
+        const labels = element.toArray()
+            .map(str => { return $(str).text().trim()})
+            .filter(value => !(value.includes("RU - I") || value.includes("RU - II") || value.includes("RU III")))
+            .map(str => str.replace(';', ""))
+
+        // Tratamento de dados desnecessário, mas útil caso separemos a resposta no json em componentes da refeição
+        const matches = labels.map((value) => {
+            const splitted = value.split(' ').map(str => str.trim())
+            if (value.includes("Cafe")) {
+                indexes.push(labels.indexOf(value))
+                return ["TIPO", splitted.slice(0, 3).join(' ').trim()]
+            } else if (value.includes("Alternativo")) {
+                indexes.push(labels.indexOf(value))
+                return ["TIPO", splitted.slice(0, 2).join(' ').trim()]
+            } else if (value.includes("Almoco") || value.includes("Jantar") || value.includes("Almoço")) {
+                indexes.push(labels.indexOf(value))
+                return ["TIPO", splitted[0]]
+            } else if  (value.startsWith(".:")) {
+                return ["OUTRO", splitted.slice(1, splitted.length).join(" ").trim()]
+            } else {
+                return value.split(separator).map((x) => x.trim())
+            }
+        })
+
+        for (let i = 1; i <= indexes.length; i++) {
+            const n = matches.slice(indexes[i - 1], indexes[i])
+            meals.push(n)
+        }
+
+        for (let meal of meals) {
+            const tipo = meal[0][1]
+            const menu = meal.slice(1, meal.length).map(entry => entry[1])
+            json.push({tipo: tipo, cardapio: menu})
+        }
+
+        return json
+    }
+
+    public async campusVicosa(url: string) {
 
         try {
             const html = await this.getHtml(url)
-            const $ = cheerio.load(html)
-            const element = $('#tbl_info tr td')
-            const labels = element.toArray().map(str => { return $(str).text().trim()}).filter(value => !(value.includes("RU - I") || value.includes("RU - II") || value.includes("RU III")))
+            const meals = this.getMeals(html)
 
-            // Tratamento de dados desnecessário, mas útil caso separemos a resposta no json em componentes da refeição
-            const matches = labels.map((value) => {
-                const splitted = value.split(' ').map(str => str.trim())
-                if (value.includes("Cafe")) {
-                    indexes.push(labels.indexOf(value))
-                    return ["TIPO", splitted.slice(0, 3).join(' ')]
-                } else if (value.includes("Alternativo")) {
-                    indexes.push(labels.indexOf(value))
-                    return ["TIPO", splitted.slice(0, 2).join(' ')]
-                } else if (value.includes("Almoco") || value.includes("Jantar")) {
-                    indexes.push(labels.indexOf(value))
-                    return ["TIPO", splitted[0]]
-                } else if  (value.startsWith(".:")) {
-                    return ["OUTRO", splitted.slice(1, splitted.length).join(" ")]
-                } else {
-                    return value.split(":").map((x) => x.trim())
-                }
-            })
-
-            for (let i = 1; i <= indexes.length; i++) {
-                const n = matches.slice(indexes[i - 1], indexes[i])
-                meals.push(n)
-            }
-
-            for (let meal of meals) {
-                const tipo = meal[0][1]
-                const menu = meal.slice(1, meal.length).map(entry => entry[1])
-                json.push({tipo: tipo, cardapio: menu})
-            }
-
-            return json
+            return meals
         } catch (error) {
             console.log(error)
         }
     }
 
     public async campusCRP(url: string) {
-        const meals: string[][][] = []
-        const json: Meal[] = []
-        const indexes: number[] = []
 
         try {
             const html = await this.getHtml(url)
-            const $ = cheerio.load(html)
-            const element = $('#tbl_info tr td')
-            const labels = element.toArray().map(str => { return $(str).text().trim()}).map(str => str.replace(';', ""))
+            const meals = this.getMeals(html, ',')
 
-            const matches = labels.map((value) => {
-                const splitted = value.split(' ').map(str => str.trim())
-                if (value.includes("Almoço") || value.includes("Jantar")) {
-                    indexes.push(labels.indexOf(value))
-                    return ["TIPO", splitted[0]]
-                } else {
-                    return value.split(",").map((x) => x.trim())
-                }
-            })
-
-            for (let i = 1; i <= indexes.length; i++) {
-                const n = matches.slice(indexes[i - 1], indexes[i])
-                meals.push(n)
-            }
-
-            for (let meal of meals) {
-                const tipo = meal[0][1]
-                const menu = meal.slice(1, meal.length).map(entry => entry[1])
-                json.push({tipo: tipo, cardapio: menu})
-            }
-
-            return json
+            return meals
         } catch (error) {
             console.log(error)
         }
