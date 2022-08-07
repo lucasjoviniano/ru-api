9b11feab644fd20d62d91e780c64592eb759643b
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-08-07 17:38:40 -0300

    Reformating

--- src/routes/campi.ts
+++ src/routes/campi.ts
@@ -1,22 +1,24 @@
-import express from 'express';
-import RUService from '../services/RUService';
+import express from "express";
+import RUService from "../services/RUService";
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/vicosa", async (request, response) => {
-    const service = new RUService()
+  const service = new RUService();
 
-    const vicosa = await service.campusVicosa("https://cardapio.ufv.br/");
+  const vicosa = await service.campusVicosa("https://cardapio.ufv.br/");
 
-    return response.json(vicosa)
-})
+  return response.json(vicosa);
+});
 
 router.get("/crp", async (request, response) => {
-    const service = new RUService()
+  const service = new RUService();
 
-    const crp = await service.campusCRP('https://sisru.crp.ufv.br/cardapioIframe.php')
+  const crp = await service.campusCRP(
+    "https://sisru.crp.ufv.br/cardapioIframe.php"
+  );
 
-    return response.json(crp)
-})
+  return response.json(crp);
+});
 
-export default router
+export default router;
--- src/services/RUService.ts
+++ src/services/RUService.ts
@@ -1,100 +1,108 @@
-import cheerio from 'cheerio';
-import * as puppeteer from 'puppeteer';
-import type Meal from '../models/meal'
+import cheerio from "cheerio";
+import * as puppeteer from "puppeteer";
+import type Meal from "../models/meal";
 
 class RUService {
+  private async getHtml(url: string) {
+    // Argumentos utilizados para funcionar corretamente no Heroku.
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
 
-    private async getHtml(url: string) {
-        // Argumentos utilizados para funcionar corretamente no Heroku.
-        const browser = await puppeteer.launch({
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-              ],
-        });
-        const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(".titulo_composicao");
+    const pageData = await page.evaluate(() => {
+      return {
+        html: document.documentElement.innerHTML,
+      };
+    });
 
-        await page.goto(url);
-        await page.waitForSelector('.titulo_composicao')
-        const pageData = await page.evaluate(() => {
-            return {
-                html: document.documentElement.innerHTML
-            }
-        })
+    browser.close();
 
-        browser.close();
+    return pageData.html;
+  }
 
-        return pageData.html
+  private getMeals(html: string, separator: string = ":") {
+    const indexes: number[] = [];
+    const meals: string[][][] = [];
+    const json: Meal[] = [];
+
+    const $ = cheerio.load(html);
+    const element = $("#tbl_info tr td");
+    const labels = element
+      .toArray()
+      .map((str) => {
+        return $(str).text().trim();
+      })
+      .filter(
+        (value) =>
+          !(
+            value.includes("RU - I") ||
+            value.includes("RU - II") ||
+            value.includes("RU III")
+          )
+      )
+      .map((str) => str.replace(";", ""));
+
+    // Tratamento de dados desnecessário, mas útil caso separemos a resposta no json em componentes da refeição
+    const matches = labels.map((value) => {
+      const splitted = value.split(" ").map((str) => str.trim());
+      if (value.includes("Cafe")) {
+        indexes.push(labels.indexOf(value));
+        return ["TIPO", splitted.slice(0, 3).join(" ").trim()];
+      } else if (value.includes("Alternativo")) {
+        indexes.push(labels.indexOf(value));
+        return ["TIPO", splitted.slice(0, 2).join(" ").trim()];
+      } else if (
+        value.includes("Almoco") ||
+        value.includes("Jantar") ||
+        value.includes("Almoço")
+      ) {
+        indexes.push(labels.indexOf(value));
+        return ["TIPO", splitted[0]];
+      } else if (value.startsWith(".:")) {
+        return ["OUTRO", splitted.slice(1, splitted.length).join(" ").trim()];
+      } else {
+        return value.split(separator).map((x) => x.trim());
+      }
+    });
+
+    for (let i = 1; i <= indexes.length; i++) {
+      const n = matches.slice(indexes[i - 1], indexes[i]);
+      meals.push(n);
     }
 
-    private getMeals(html: string, separator: string = ':') {
-        const indexes: number[] = []
-        const meals: string[][][] = []
-        const json: Meal[] = []
-
-        const $ = cheerio.load(html)
-        const element = $('#tbl_info tr td')
-        const labels = element.toArray()
-            .map(str => { return $(str).text().trim()})
-            .filter(value => !(value.includes("RU - I") || value.includes("RU - II") || value.includes("RU III")))
-            .map(str => str.replace(';', ""))
-
-        // Tratamento de dados desnecessário, mas útil caso separemos a resposta no json em componentes da refeição
-        const matches = labels.map((value) => {
-            const splitted = value.split(' ').map(str => str.trim())
-            if (value.includes("Cafe")) {
-                indexes.push(labels.indexOf(value))
-                return ["TIPO", splitted.slice(0, 3).join(' ').trim()]
-            } else if (value.includes("Alternativo")) {
-                indexes.push(labels.indexOf(value))
-                return ["TIPO", splitted.slice(0, 2).join(' ').trim()]
-            } else if (value.includes("Almoco") || value.includes("Jantar") || value.includes("Almoço")) {
-                indexes.push(labels.indexOf(value))
-                return ["TIPO", splitted[0]]
-            } else if  (value.startsWith(".:")) {
-                return ["OUTRO", splitted.slice(1, splitted.length).join(" ").trim()]
-            } else {
-                return value.split(separator).map((x) => x.trim())
-            }
-        })
-
-        for (let i = 1; i <= indexes.length; i++) {
-            const n = matches.slice(indexes[i - 1], indexes[i])
-            meals.push(n)
-        }
-
-        for (let meal of meals) {
-            const tipo = meal[0][1]
-            const menu = meal.slice(1, meal.length).map(entry => entry[1])
-            json.push({tipo: tipo, cardapio: menu})
-        }
-
-        return json
+    for (let meal of meals) {
+      const tipo = meal[0][1];
+      const menu = meal.slice(1, meal.length).map((entry) => entry[1]);
+      json.push({ tipo: tipo, cardapio: menu });
     }
 
-    public async campusVicosa(url: string) {
+    return json;
+  }
 
-        try {
-            const html = await this.getHtml(url)
-            const meals = this.getMeals(html)
+  public async campusVicosa(url: string) {
+    try {
+      const html = await this.getHtml(url);
+      const meals = this.getMeals(html);
 
-            return meals
-        } catch (error) {
-            console.log(error)
-        }
+      return meals;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    public async campusCRP(url: string) {
+  public async campusCRP(url: string) {
+    try {
+      const html = await this.getHtml(url);
+      const meals = this.getMeals(html, ",");
 
-        try {
-            const html = await this.getHtml(url)
-            const meals = this.getMeals(html, ',')
-
-            return meals
-        } catch (error) {
-            console.log(error)
-        }
+      return meals;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
-export default RUService
+export default RUService;
